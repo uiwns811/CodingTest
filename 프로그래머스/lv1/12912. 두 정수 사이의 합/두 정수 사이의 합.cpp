7ab722cf3412ba69f11ea5c47eb43db6ddbbc95f
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-03-15 19:01:45 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    int min, max;
+    
+    if (a == b) {
+        return a;
+    }
+    
+    if (a > b) {
+        min = b;
+        max = a;
+    }
+    else {
+        min = a;
+        max = b;
+    }
+    
+    for (int i = min ; i <= max ; ++i) {
+        answer += i;
+    }
+    
+    return answer;
+}
