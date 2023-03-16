1640138155ccbc437466d1dc26ec8c8a109ceca6
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-03-16 22:40:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,44 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-
-// 공백 공백 = 공백
-// 공백 벽 = 벽
-// 벽 벽 = 벽
-
-vector<int> GetBinary(int n, int num) {
-    vector<int> binary;
-    
-    while(num > 1) {
-        binary.emplace_back(num % 2);
-        num /= 2;
-    }
-    binary.emplace_back(num);
-    
-    while(binary.size() != n) {
-        binary.emplace_back(0);
-    }
-    
-    reverse(binary.begin(), binary.end());
-    return binary;
-    
-}
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
     
-    for (int j = 0 ; j < n ; j++) {
-        string line = "";
-        vector<int> a = GetBinary(n, arr1[j]);
-        vector<int> b = GetBinary(n, arr2[j]);
-
-        for (int k = 0 ; k < n ; k++) {
-            string space = (a[k] == 0 && b[k] == 0 ? " " : "#");
-            line += space;
+    for (int i = 0 ; i < n ; i++) {
+        arr1[i] = arr1[i] | arr2[i];        // arr1과 arr2의 비트연산한 값이 나옴
+        string line;
+        
+        while(line.length() != n) {
+            if (arr1[i] % 2 == 0) line = " " + line;
+            else line = "#" + line;
+            arr1[i] /= 2;
         }
+        
+        
         answer.emplace_back(line);
     }
+    
     return answer;
 }
