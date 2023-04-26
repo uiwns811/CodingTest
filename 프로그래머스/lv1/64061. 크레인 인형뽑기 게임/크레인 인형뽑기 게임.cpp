9dde4e0e2d97c9ba44e9126e8d5a493c8ea7eca9
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-04-26 16:01:13 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -7,31 +7,23 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    vector<vector<int>> lines;
     stack<int> buckets;
-    
-    for (int i = 0 ; i < board.size() ; i++) {
-        vector<int> vec;
-        for (int j = 0 ; j < board.size() ; j++) {
-            if (board[j][i] != 0) {
-                vec.emplace_back(board[j][i]);
-            }
-        }
-        lines.emplace_back(vec);
-    }
-    
+
     for (auto& m : moves) {
-        if (lines[m-1].empty()) continue;
-        int doll = lines[m-1][0];
-        lines[m-1].erase(lines[m-1].begin(), lines[m-1].begin() + 1);
-        
-        if (buckets.empty()) buckets.push(doll);
-        else if (buckets.top() != doll) buckets.push(doll);
-        else {
-            buckets.pop();
-            answer += 2;
-        }
+        for (int j = 0 ; j < board.size() ; j++ ) {
+            int doll = board[j][m-1];
+            if (doll == 0) continue;
+            else {
+                board[j][m-1] = 0;
+                if (buckets.empty()) buckets.push(doll);
+                else if (buckets.top() != doll) buckets.push(doll);
+                else {
+                    buckets.pop();
+                    answer += 2;
+                }
+                break;
+            }
+        }  
     }
-    
     return answer;
 }
