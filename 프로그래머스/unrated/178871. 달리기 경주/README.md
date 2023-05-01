0686bc2f32beec1d444244ea6911f33b8dba5c2f
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-05-01 22:34:24 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/README.md
+++ 프로그래머스/unrated/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [unrated] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871#) 
 
 ### 성능 요약
 
-메모리: 4.05 MB, 시간: 0.02 ms
+메모리: 4.14 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.cpp
@@ -10,23 +10,22 @@ using namespace std;
 vector<string> solution(vector<string> players, vector<string> callings) {
     vector<string> answer;
     unordered_map<string, int> umsi;
-    map<int, string> umis;
+    map<int, string> mis;
     
     for (int i = 0 ; i < players.size() ; i++) {
         umsi[players[i]] = i+1;
-        umis[i+1] = players[i];
+        mis[i+1] = players[i];
     }
     
     for (auto& call : callings) {
     	int idx = umsi[call];
-        string p = umis[idx-1];
-        umsi[call] -= 1;    
-        umsi[p] += 1;
-        umis[idx] = p;
-        umis[idx-1] = call;
+        string p = mis[idx-1];
+
+        swap(umsi[call], umsi[p]);
+        swap(mis[idx], mis[idx-1]);
     }
     
-    for (auto& um : umis) {
+    for (auto& um : mis) {
         answer.emplace_back(um.second);
     }
 
