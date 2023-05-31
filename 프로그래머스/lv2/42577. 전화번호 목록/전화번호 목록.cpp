01d0f53668c77b3f8193ff6aaa35a33475d3475d
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-05-31 13:00:59 +0900

    [level 2] Title: 전화번호 목록, Time: 1708.05 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,19 +1,27 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    unordered_map<string, int> um;
     
-    sort(phone_book.begin(), phone_book.end());    
+    for(auto& pb : phone_book) {
+        um[pb] = 1;
+    }
     
-    for (int i = 0 ; i < phone_book.size() - 1 ; i++) {
-        if (phone_book[i] == phone_book[i+1].substr(0, phone_book[i].length())) {
-            answer = false;
-            break;
+    for (int i = 0 ; i < phone_book.size() ; i++) {
+        string str = "";
+        for (int j = 0 ; j < phone_book[i].length() ; j++) {
+            str += phone_book[i][j];
+            if (um[str] && str != phone_book[i]) {
+                answer = false;
+                break;
+			}
         }
     }
+    
     return answer;
 }
