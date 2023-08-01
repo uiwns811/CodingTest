012cfe0c776aab78338c758b03b522842ab0a43f
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-08-01 15:12:11 +0900

    [level 2] Title: 타겟 넘버, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -5,26 +5,23 @@
 using namespace std;
 
 int answer = 0;
-vector<int> nums;
 
-void GetNum(int curNum, int idx, int target, int size) 
+void GetNum(int curNum, int idx, int target, vector<int>& vec) 
 {
-    //cout << curNum << ", " << idx << endl;
-    if (idx == size - 1) {
+    if (idx == vec.size() - 1) {
         if (curNum == target) {
             answer++;
         }
         return;
     }
-    GetNum(curNum + nums[idx + 1], idx + 1, target, size);
-    GetNum(curNum - nums[idx + 1], idx + 1, target, size);
+    GetNum(curNum + vec[idx + 1], idx + 1, target, vec);
+    GetNum(curNum - vec[idx + 1], idx + 1, target, vec);
 }
 
-int solution(vector<int> numbers, int target) {    
-    nums = numbers;
+int solution(vector<int> numbers, int target) { 
     
-    GetNum(nums[0], 0, target, numbers.size());
-    GetNum(-nums[0], 0, target, numbers.size());
+    GetNum(numbers[0], 0, target, numbers);
+    GetNum(-numbers[0], 0, target, numbers);
    
     return answer;
 }
