446b6ffb5f1c084936650dd1b674561d00e3829b
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-08-02 14:33:35 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool visited[201] {false};
+int connected = 0;
+
+void dfs(vector<vector<int>>& com, int n, int cur)
+{
+    if (cur >= n) {
+        return;
+    }
+    visited[cur] = true;
+    
+    for(int i = 0 ; i < n ; i++) {
+        if (!visited[i] && com[cur][i] == 1) {
+            dfs(com, n, i);
+        }
+    }    
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    for (int i = 0 ; i < n ; i++) {
+        if (!visited[i]) {
+    		dfs(computers, n, i);
+            answer++;
+        }
+    }
+    return answer;
+}
