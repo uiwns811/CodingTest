d80596a72524e0ec37f95e4951ed225c74ed2229
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-09-21 15:46:10 +0900

    [Silver III] Title: 에너지 드링크, Time: 28 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
@@ -5,23 +5,27 @@ using namespace std;
 
 int main()
 {
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
 	int n;
 	cin >> n;
 
 	double num;
-	vector<double> v;
+	vector<double> vec;
 	double start = 1;
 	for (int i = 0; i < n; i++) {
 		cin >> num;
-		v.push_back(num);
+		vec.push_back(num);
 		start = max(start, num);
 	}
 
 	double answer = 0;
 	answer += start/2;
 	
-	for (int i = 0; i < v.size(); i++) {
-		answer += v[i] / 2;
+	for (auto& v : vec) {
+		answer += v / 2;
 	}
 	cout << answer << endl;
 }
