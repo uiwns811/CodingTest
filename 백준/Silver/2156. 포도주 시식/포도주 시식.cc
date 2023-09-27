056605dc6196e70712ce2c25f4826680b2537abf
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2023-09-27 15:53:05 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[10'005];
+int dp[10'005];
+
+int main()
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) 
+		cin >> arr[i];
+
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 1], max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+	}
+
+	cout << dp[n] << endl;
+}
